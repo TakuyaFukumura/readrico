9b3a0b7104e75ad03619f1f4c075a8f54ab24b0f
takuya-fukumura <blackchoco1114@gmail.com>
2025-07-24 18:30:00 +0900

    不要になった初期コード削除

--- src/main/java/com/example/myapplication/controller/IndexController.java
+++ src/main/java/com/example/myapplication/controller/IndexController.java
@@ -1,26 +1,16 @@
 package com.example.myapplication.controller;
 
-import com.example.myapplication.service.IndexService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/") // URLとの関連付け http://localhost:8080/ の時に呼ばれる
+@RequestMapping("/")
 public class IndexController {
 
-    private final IndexService service;
-
-    @Autowired
-    public IndexController(IndexService service) {
-        this.service = service; // IndexServiceのインスタンスを使えるようにしている
-    }
-
-    @GetMapping // Getされた時の処理 Postは別
+    @GetMapping
     public String index(Model model) {
-        model.addAttribute("message", service.getMessage());
         return "index";
     }
 }
