deeb069e3a8d9fdc655db0fe4f7d9b1a67d88ab1
takuya-fukumura <blackchoco1114@gmail.com>
2025-07-28 22:34:50 +0900

    import整理＆整形

--- src/main/java/com/example/myapplication/util/TempMultipartFile.java
+++ src/main/java/com/example/myapplication/util/TempMultipartFile.java
@@ -2,7 +2,10 @@ package com.example.myapplication.util;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * 一時的なMultipartFileの実装
@@ -19,30 +22,42 @@ public class TempMultipartFile implements MultipartFile {
     }
 
     @Override
-    public String getName() { return name; }
-
-    @Override
-    public String getOriginalFilename() { return name; }
-
-    @Override
-    public String getContentType() { return contentType; }
-
-    @Override
-    public boolean isEmpty() { return content.length == 0; }
-
-    @Override
-    public long getSize() { return content.length; }
-
-    @Override
-    public byte[] getBytes() { return content; }
-
-    @Override
-    public java.io.InputStream getInputStream() {
-        return new java.io.ByteArrayInputStream(content);
+    public String getName() {
+        return name;
     }
 
     @Override
-    public void transferTo(java.io.File dest) throws IOException {
+    public String getOriginalFilename() {
+        return name;
+    }
+
+    @Override
+    public String getContentType() {
+        return contentType;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return content.length == 0;
+    }
+
+    @Override
+    public long getSize() {
+        return content.length;
+    }
+
+    @Override
+    public byte[] getBytes() {
+        return content;
+    }
+
+    @Override
+    public InputStream getInputStream() {
+        return new ByteArrayInputStream(content);
+    }
+
+    @Override
+    public void transferTo(File dest) throws IOException {
         throw new UnsupportedOperationException();
     }
 }
